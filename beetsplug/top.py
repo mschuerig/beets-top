9d0e10c815367d3241755866dd72fef0b082fa3b
Michael Schuerig <michael@schuerig.de>
2013-04-21 17:23:32 +0200

    Use proper 4 space indentation.

--- beetsplug/top.py
+++ beetsplug/top.py
@@ -24,26 +24,26 @@ from string import Template
 import logging
 
 SQL = Template(
-  "select $what, count(*) as albums, sum(tracks) as tracks, sum(length) as time from "
-  "(select $what, album, count(*) as tracks, sum(length) as length from items $where group by $what, album) "
-  "group by $what "
-  "order by $order desc "
-  "limit $count"
+    "select $what, count(*) as albums, sum(tracks) as tracks, sum(length) as time from "
+    "(select $what, album, count(*) as tracks, sum(length) as length from items $where group by $what, album) "
+    "group by $what "
+    "order by $order desc "
+    "limit $count"
 )
 
 WHATS = {
-  "artists":      "artist",
-  "albumartists": "albumartist",
-  "composers":    "composer",
-  "formats":      "format",
-  "genres":       "genre",
-  "labels":       "label",
-  "years":        "year"
+    "artists":      "artist",
+    "albumartists": "albumartist",
+    "composers":    "composer",
+    "formats":      "format",
+    "genres":       "genre",
+    "labels":       "label",
+    "years":        "year"
 }
 ORDERS = {
-  "albums": "albums",
-  "tracks": "tracks",
-  "time":   "time"
+    "albums": "albums",
+    "tracks": "tracks",
+    "time":   "time"
 }
 DEFAULT_COUNT = 10
 DEFAULT_ORDER = "albums"
@@ -51,51 +51,51 @@ DEFAULT_ORDER = "albums"
 log = logging.getLogger('beets')
 
 def top(lib, args, opts):
-  params = parse_args(args)
-  q = SQL.substitute(params)
-  log.debug(q)
-  with lib.transaction() as tx:
-    rows = tx.query(q)
-  print_result(rows, opts)
+    params = parse_args(args)
+    q = SQL.substitute(params)
+    log.debug(q)
+    with lib.transaction() as tx:
+        rows = tx.query(q)
+    print_result(rows, opts)
 
 def print_result(rows, opts):
-  for what, albums, tracks, time in rows:
-    print "%s - %d - %d - %s" % (what, albums, tracks, timedelta(seconds=int(time)))
+    for what, albums, tracks, time in rows:
+        print "%s - %d - %d - %s" % (what, albums, tracks, timedelta(seconds=int(time)))
   
 def parse_args(args):
-  what  = None
-  where = None
-  count = DEFAULT_COUNT
-  order = DEFAULT_ORDER
-  if len(args) >= 1:
-    if args[0].isdigit():
-      count = int(args[0])
-      args = args[1:]
-  if len(args) >= 1:
-    what, args = WHATS.get(args[0]), args[1:]
-  if len(args) >= 2 and args[0] == "by":
-    args = args[1:]
-  if len(args) >= 1:
-    order, args = ORDERS.get(args[0]), args[1:]
-  if len(args) >= 2 and args[0] == "in":
-    args = args[1:]
-  if len(args) >= 1:
-    years, args = expand_years(args[0]), args[1:]
-    where = "where year in (%s)" % ','.join(years)
-  
-  if not what or not order or not count:
-    raise ui.UserError(u'invalid arguments')
-  return { 'what': what, 'where': where, 'order': order, 'count': count }
+    what  = None
+    where = None
+    count = DEFAULT_COUNT
+    order = DEFAULT_ORDER
+    if len(args) >= 1:
+        if args[0].isdigit():
+            count = int(args[0])
+            args = args[1:]
+    if len(args) >= 1:
+        what, args = WHATS.get(args[0]), args[1:]
+    if len(args) >= 2 and args[0] == "by":
+        args = args[1:]
+    if len(args) >= 1:
+        order, args = ORDERS.get(args[0]), args[1:]
+    if len(args) >= 2 and args[0] == "in":
+        args = args[1:]
+    if len(args) >= 1:
+        years, args = expand_years(args[0]), args[1:]
+        where = "where year in (%s)" % ','.join(years)
+
+    if not what or not order or not count:
+        raise ui.UserError(u'invalid arguments')
+    return { 'what': what, 'where': where, 'order': order, 'count': count }
 
 def expand_years(years_spec):
-  ranges = [[int(y) for y in se.split('-')] for se in years_spec.split(',')]
-  years = [range(r[0], r[1] + 1) if len(r) > 1 else [r[0]] for r in ranges]
-  return [str(y) for yrs in years for y in yrs]
+    ranges = [[int(y) for y in se.split('-')] for se in years_spec.split(',')]
+    years = [range(r[0], r[1] + 1) if len(r) > 1 else [r[0]] for r in ranges]
+    return [str(y) for yrs in years for y in yrs]
   
 class TopPlugin(BeetsPlugin):
-  def commands(self):
-    cmd = ui.Subcommand('top', help='show top artists, genres, years, or labels')
-    def func(lib, opts, args):
-      top(lib, args, opts)
-    cmd.func = func
-    return [cmd]
+    def commands(self):
+        cmd = ui.Subcommand('top', help='show top artists, genres, years, or labels')
+        def func(lib, opts, args):
+            top(lib, args, opts)
+        cmd.func = func
+        return [cmd]
